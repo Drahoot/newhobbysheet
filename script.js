9bc3cd2f980cf2703ab9874fe2f790cc37d4e7fc
Ryan <superryanman1@gmail.com>
2022-01-27 15:39:37 -0800

    modified js file and added a loop to make my functions operate

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let PageANS = prompt('Music, Traveling, Hunting, Reading; What sounds the most appealing out of these four?')
+let vxerror = true
+while (vxerror === true)
+{let PageANS = prompt('Music, Traveling, Hunting, Reading; What sounds the most appealing out of these four?')
 
 if (PageANS === 'Music') {
 alert ('You are in the right place so far!')
-} 
+let MANS = prompt('Who is your favorite band or artist')
+ 
+ alert ('No way! I love to listen to ' + MANS + ' as well!')
+vxerror = false;
+    } 
 else if (PageANS === 'Traveling') {
     alert ('Id fly over to Bishals page! It can be accessed via the TOC!')
-}
+    vxerror = false;
+    }
 else if (PageANS === 'Hunting') {
     alert ('Tracks lead back to Jons page! It can be accessed via the TOC!')
-}
+    vxerror = false;
+    }
 else if (PageANS === 'Reading') {
 alert ('Cant process late fees here, check you Coles page! it can be accessed via the TOC!')
-}
+vxerror = false;
+    }
  else 
- {alert ('Whoopsie daisy there was an error!')}
+ {alert ('Whoopsie daisy there was an error!')
+ vxerror = true;}
+}
 
 
- if (PageANS === 'Music'){
-     let MANS = prompt('Who is your favorite band or artist')
+
+
  
- alert ('No way! I love to listen to ' + MANS + ' as well!')}
