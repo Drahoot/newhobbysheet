1deb386ce87b866a42fe5b3d05cba23fd831e83b
Ryan <superryanman1@gmail.com>
2022-01-26 16:06:43 -0800

    added a js page and new links to classmate pages

---
+++ script.js
@@ -0,0 +1,22 @@
+let PageANS = prompt('Music, Traveling, Hunting, Reading; What sounds the most appealing out of these four?')
+
+if (PageANS === 'Music') {
+alert ('You are in the right place so far!')
+} 
+else if (PageANS === 'Traveling') {
+    alert ('Id fly over to Bishals page! It can be accessed via the TOC!')
+}
+else if (PageANS === 'Hunting') {
+    alert ('Tracks lead back to Jons page! It can be accessed via the TOC!')
+}
+else if (PageANS === 'Reading') {
+alert ('Cant process late fees here, check you Coles page! it can be accessed via the TOC!')
+}
+ else 
+ {alert ('Whoopsie daisy there was an error!')}
+
+
+ if (PageANS === 'Music'){
+     let MANS = prompt('Who is your favorite band or artist')
+ 
+ alert ('No way! I love to listen to ' + MANS + ' as well!')}
